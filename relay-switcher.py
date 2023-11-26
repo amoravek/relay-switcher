8487ec0003297f4765da74d2a7c20efa4d3207ee
--edit <--edit>
2023-11-26 21:42:43 +0100

    updates

--- relay-switcher.py
+++ relay-switcher.py
@@ -101,13 +101,14 @@ def start_periodic_task():
     except Exception as e:
         logger.error(traceback.format_exc())
 
+    threading.Timer(UPDATE_DELAY_SECS, start_periodic_task).start()
+
 if __name__ == '__main__':
     try:
         # GPIO.setmode(GPIO.BCM)
         # GPIO.setup(RELAY_PIN, GPIO.OUT)
         # GPIO.output(RELAY_PIN, 0)
         logger.addHandler(handler)
-        threading.Timer(UPDATE_DELAY_SECS, start_periodic_task).start()
         start_periodic_task()
         app.run(debug=False, host='0.0.0.0', port=APP_PORT)
     except Exception as e:
